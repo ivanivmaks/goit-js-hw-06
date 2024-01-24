6bbac63968bb029f5f2d2c4c43335592b1cd0de4
ivaniv_maks <ivanivmaks0@gmail.com>
2024-01-24 18:43:51 +0200

    commit

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,36 @@
+const create = document.querySelector("[data-create]");
+const destroy = document.querySelector("[data-destroy]");
+const boxes = document.querySelector("#boxes");
+const input = document.querySelector('input[type="number"]');
+
+function createBox(size) {
+  const box = document.createElement("div");
+  box.style.width = `${size}px`;
+  box.style.height = `${size}px`;
+  box.style.backgroundColor = getRandomHexColor();
+  boxes.appendChild(box);
+}
+
+function createBoxes(amount) {
+  destroyBoxes();
+  let size = 30;
+  for (let i = 0; i < amount; i += 1) {
+    createBox(size);
+    size += 10;
+  }
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+create.addEventListener("click", function () {
+  createBoxes(input.value);
+});
+destroy.addEventListener("click", destroyBoxes);
